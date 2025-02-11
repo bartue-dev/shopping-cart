be9c1e0ad84cfd339c3baa86f87e0fa448b658f5
Bartue <[bartbatiancila28@gmail.com]>
2025-02-11 23:20:45 +0800

    Added a cartItems state that handle the cart data

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Navbar from "./Components/Navbar"
 function App() {
   //declare product details state here to pass it on the Product-items and Proudct Details Component (unrelated components)
   const [productDetails, setProductDetails] = useState()
+  const [cartItems, setCartItems] = useState([]);
 
   return (
     <div>
-      <Navbar />
-      <Outlet context={[productDetails, setProductDetails]}/>
+      <Navbar cartItems={cartItems}/>
+      <Outlet context={[productDetails, setProductDetails, cartItems, setCartItems]}/>
     </div>
   )
 }
