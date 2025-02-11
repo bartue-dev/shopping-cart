a8ade95dbb76ae1eb6495fe216bad7a9c5cbd5ce
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-02-11 23:26:41 +0800

    Remove custom hook useProductDetails cause it is uncessary

--- src/utils/useProductDetails.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-function useProductDetails() {
-  const [productDetails, setProductDetails] = useState([]);
-
-  return {productDetails, setProductDetails}
-}
-
-export default useProductDetails
