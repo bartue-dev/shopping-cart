d5723fcfd496fdf52c3bd0996c886e82a56baea5
Bartue <[bartbatiancila28@gmail.com]>
2025-02-11 12:13:07 +0800

    Remove the array from the useState to handle no data error when the page refreshes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Components/Navbar"
 
 function App() {
   //declare product details state here to pass it on the Product-items and Proudct Details Component (unrelated components)
-  const [productDetails, setProductDetails] = useState([])
+  const [productDetails, setProductDetails] = useState()
 
   return (
     <div>
