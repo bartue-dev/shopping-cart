e9e8abf596767bedf1d19c333a2b4a8ccdb88f69
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-02-13 23:58:34 +0800

    Added a cart empty component that render if the cartItems is empty

---
+++ src/Components/Cart-empty.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+function CartEmpty() {
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-15 z-1 mt-15">
+      <h1 className="text-5xl">Opps!</h1>
+      <p className="text-xl italic">Your Cart is empty!</p>
+      <Link to="/products" className="underline">go back to products</Link>
+    </div>
+  )
+}
+
+export default CartEmpty
