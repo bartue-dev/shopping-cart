b0821c082ba56ce8684f2db36052ad0c16f8ca23
Bartue <[bartbatiancila28@gmail.com]>
2025-02-14 00:02:14 +0800

    Edit the cart-empty component. Only cart is empty text displays

--- src/Components/Cart-empty.jsx
+++ src/Components/Cart-empty.jsx
@@ -4,7 +4,6 @@ function CartEmpty() {
 
   return (
     <div className="flex flex-col items-center justify-center gap-15 z-1 mt-15">
-      <h1 className="text-5xl">Opps!</h1>
       <p className="text-xl italic">Your Cart is empty!</p>
       <Link to="/products" className="underline">go back to products</Link>
     </div>
