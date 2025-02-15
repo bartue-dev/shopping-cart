dacf666b7c91477297a08dc67d7b2fe0910c94e9
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-02-15 21:52:18 +0800

    Removed the w-250 for <p> parent container cause it causing a layout issues

--- src/Pages/Home/Home-page.jsx
+++ src/Pages/Home/Home-page.jsx
@@ -7,8 +7,8 @@ function HomePage() {
       {/* background image */}
       <BackgroundImage />
 
-      <div className="flex items-center justify-center max-w-4xl z-1 ">
-        <div className="mb-30 flex flex-col gap-10 w-250">
+      <div className="flex items-center justify-center w-4xl z-1 ">
+        <div className="mb-30 flex flex-col gap-10">
           <h1 className="text-5xl font-bold">Get your Outfit with <span className="italic">anyWEAR</span></h1>
           <p className="text-wrap">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae minima recusandae, quam praesentium exercitationem nam ab, eveniet dolorem, id deleniti alias debitis fuga ut ratione accusamus esse mollitia error earum.
